e4e64519b1951f5ede5bfee3454d0bde00402bbb
StanislasBASQUIN <stanislas.basquin@gmail.com>
2020-12-02 16:05:31 +0100

    Adding Revocation Stragegie to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Adding JwtDenyList Revocation Strategie
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
