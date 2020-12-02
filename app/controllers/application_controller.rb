04afca8c35e3725cb7c41e1bdaade4a26a0d02d9
StanislasBASQUIN <stanislas.basquin@gmail.com>
2020-12-02 16:42:15 +0100

    Creating Manual API Routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  
+  def render_jsonapi_response(resource)
+    if resource.errors.empty?
+      render jsonapi: resource
+    else
+      render jsonapi_errors: resource.errors, status: 400
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  # To avoid conflicts with Devise's own namespacing logic we'll manually set our authentication routes
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: %w[show]
+  end
+  
+  devise_for :users,
+    defaults: { format: :json },
+    path: '',
+    path_names: {
+      sign_in: 'api/login',
+      sign_out: 'api/logout',
+      registration: 'api/signup'
+    },
+    controllers: {
+      sessions: 'sessions',
+      registrations: 'registrations'
+    }
+
   root 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
