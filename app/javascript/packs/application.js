dfc02c6dbe75e2dc992b3eb6c491edc89e4a58b1
oumasma <oumasma93@hotmail.fr>
2020-12-02 15:53:46 +0400

    add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+require('bootstrap')
+import 'bootstrap/dist/css/bootstrap'
