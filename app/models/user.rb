46bd3b2399ea890bacf77f5d99f723e4dae67ac7
Charlie Carpene <solunacisv@gmail.com>
2020-12-02 18:43:50 +0100

    add , to user Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
   # Adding JwtDenyList Revocation Strategie
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
